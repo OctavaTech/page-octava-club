a66dc0c09427b71bdd4f18779d6ad7717e6f1a7a
LuisRodriguez <luisrodriguezacosta@gmail.com>
2025-07-17 21:36:20 -0500

    update name img slider

--- src/app/components/HeroSlides.tsx
+++ src/app/components/HeroSlides.tsx
@@ -8,8 +8,8 @@ const HeroSlides: React.FC = () => {
       {/* Slide 4 */}
       <div className="absolute inset-0 animate-slideshow-bg opacity-100">
         <Image
-          src="/Slide-4.jpg"
-          alt="Slide 4"
+          src="/slide-4.jpg"
+          alt="slide 4"
           fill
           priority
           className="object-cover"
@@ -20,8 +20,18 @@ const HeroSlides: React.FC = () => {
       {/* Slide 5 */}
       <div className="absolute inset-0 animate-slideshow-bg-delayed opacity-0">
         <Image
-          src="/Slide-5.jpg"
-          alt="Slide 5"
+          src="/slide-5.jpg"
+          alt="slide 5"
+          fill
+          priority
+          className="object-cover"
+          sizes="100vw"
+        />
+      </div>
+      <div className="absolute inset-0 animate-slideshow-bg-delayed opacity-0">
+        <Image
+          src="/slide-6.jpg"
+          alt="slide 6"
           fill
           priority
           className="object-cover"
